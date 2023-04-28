d8ad756235070cb8b6afda206609d6ac55e07f91
0gust <87370918+0gust@users.noreply.github.com>
2023-04-28 12:54:24 +0200

    find polizza

--- bepspidertin.py
+++ bepspidertin.py
@@ -10,4 +10,19 @@ service = FirefoxService(executable_path=GeckoDriverManager().install())
 driver = webdriver.Firefox(service=service)
 driver.get(url)
 
-usertime = driver.find_element(By)
+usertim = driver.find_element(By.NAME,"email")
+passtim = driver.find_element(by.NAME, "password")
+search = driver.find_element(By.NAME, "search")
+customer = "IACCIO"
+
+usertim.send_keys("ye00_tinbroker")
+passtim.send_keys("}Ub3t9ke6=")
+search = send_keys(customer)
+
+
+
+polizza_f = driver.find_element(By.XPATH, "//*[@id='kt_content_container']/div/div[1]/div/div/div/div[1]/div/div/div[1]/div")
+
+print(polizza_f)
+
+
